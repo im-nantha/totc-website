8fec04ac7354b95840e6d125dfe8ce1918e14579
Nanthakumar R <rnantha2002@gmail.com>
2024-08-23 18:32:08 +0530

    bug fixes and scoping js for pagination

--- js/script.js
+++ js/script.js
@@ -6,34 +6,34 @@ $(document).ready(function() {
 
     function showPage(page) {
         // Hide all cards
-        $('#cards-container .page-cards').hide();
+        $('#page-cards-container .page-cards').hide();
 
         // Calculate start and end index for the current page
         const startIndex = (page - 1) * cardsPerPage;
         const endIndex = startIndex + cardsPerPage;
 
         // Show cards for the current page
-        $('#cards-container .page-cards').slice(startIndex, endIndex).show();
+        $('#page-cards-container .page-cards').slice(startIndex, endIndex).show();
 
         // Update the active page number
-        $('.page-number').removeClass('active');
-        $(`.page-number[data-page=${page}]`).addClass('active');
+        $('#pagination .page-number').removeClass('active');
+        $(`#pagination .page-number[data-page=${page}]`).addClass('active');
     }
 
     // Handle click on page numbers
-    $('.page-number').click(function() {
+    $('#pagination .page-number').click(function() {
         currentPage = $(this).data('page');
         showPage(currentPage);
     });
 
     // Handle Prev button click
-    $('#prev').click(function() {
+    $('#pagination #prev').click(function() {
         currentPage = (currentPage === 1) ? totalPages : currentPage - 1;
         showPage(currentPage);
     });
 
     // Handle Next button click
-    $('#next').click(function() {
+    $('#pagination #next').click(function() {
         currentPage = (currentPage === totalPages) ? 1 : currentPage + 1;
         showPage(currentPage);
     });
